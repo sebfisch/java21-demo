3f02f8a3486ea3f93c949b1ca9991d0839f9ee94
Sebastian Fischer <mail@sebfisch.de>
2023-10-30 13:18:15 +0100

    use local expr declarations

--- src/main/java/sebfisch/expressions/Evaluation.java
+++ src/main/java/sebfisch/expressions/Evaluation.java
@@ -3,15 +3,6 @@ package sebfisch.expressions;
 import java.util.function.IntFunction;
 import java.util.function.IntUnaryOperator;
 
-import sebfisch.expressions.data.Add;
-import sebfisch.expressions.data.Div;
-import sebfisch.expressions.data.Expr;
-import sebfisch.expressions.data.Mul;
-import sebfisch.expressions.data.Neg;
-import sebfisch.expressions.data.Num;
-import sebfisch.expressions.data.Small;
-import sebfisch.expressions.data.Sub;
-
 public final class Evaluation {
 
     private Evaluation() {
@@ -43,41 +34,41 @@ public final class Evaluation {
 
     public static Result eval(Expr expr) {
         return switch (expr) {
-            case Small.ZERO ->
+            case Expr.Small.ZERO ->
                 new Result.Ok(0);
-            case Small.ONE ->
+            case Expr.Small.ONE ->
                 new Result.Ok(1);
-            case Num(var value) ->
+            case Expr.Num(var value) ->
                 new Result.Ok(value);
-            case Neg(var nested) -> {
+            case Expr.Neg(var nested) -> {
                 try {
                     yield eval(nested).map(Math::negateExact);
                 } catch (ArithmeticException e) {
                     yield Result.Error.OVERFLOW;
                 }
             }
-            case Add(var left, var right) -> {
+            case Expr.Add(var left, var right) -> {
                 try {
                     yield eval(left).flatMap(l -> eval(right).map(r -> Math.addExact(l, r)));
                 } catch (ArithmeticException ex) {
                     yield Result.Error.OVERFLOW;
                 }
             }
-            case Sub(var left, var right) -> {
+            case Expr.Sub(var left, var right) -> {
                 try {
                     yield eval(left).flatMap(l -> eval(right).map(r -> Math.subtractExact(l, r)));
                 } catch (ArithmeticException ex) {
                     yield Result.Error.OVERFLOW;
                 }
             }
-            case Mul(var left, var right) -> {
+            case Expr.Mul(var left, var right) -> {
                 try {
                     yield eval(left).flatMap(l -> eval(right).map(r -> Math.multiplyExact(l, r)));
                 } catch (ArithmeticException ex) {
                     yield Result.Error.OVERFLOW;
                 }
             }
-            case Div(var left, var right) ->
+            case Expr.Div(var left, var right) ->
                 eval(right).flatMap(r
                 -> r == 0
                 ? Result.Error.DIV_BY_ZERO
