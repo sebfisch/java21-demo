3e9a48194c27224b3d8e146208dfab16710e5650
Sebastian Fischer <mail@sebfisch.de>
2023-11-17 13:43:15 +0100

    renamings

--- src/main/java/sebfisch/FileSearch.java
+++ src/main/java/sebfisch/FileSearch.java
@@ -10,14 +10,14 @@ import java.util.stream.Stream;
 
 import sebfisch.util.Partial;
 
-public class SrcFileSearch {
+public class FileSearch {
 
-    private record FileMatches(Path fileName, List<String> matchingLines) {
+    private record FileMatches(Path filePath, List<String> matchingLines) {
 
-        static Partial<FileMatches, IOException> from(Path fileName, Predicate<String> isMatching) {
-            try (Stream<String> lines = Files.lines(fileName)) {
+        static Partial<FileMatches, IOException> from(Path filePath, Predicate<String> isMatching) {
+            try (Stream<String> lines = Files.lines(filePath)) {
                 List<String> matchingLines = lines.filter(isMatching).toList();
-                return new Partial.Success<>(new FileMatches(fileName, matchingLines));
+                return new Partial.Success<>(new FileMatches(filePath, matchingLines));
             } catch (IOException e) {
                 return new Partial.Failure<>(e);
             }
@@ -32,11 +32,11 @@ public class SrcFileSearch {
         try (Stream<Path> srcFiles = Files.walk(srcPath).filter(Files::isRegularFile)) {
             srcFiles
                     .map(Path::toAbsolutePath)
-                    .map(file -> FileMatches.from(file, isMatching))
+                    .map(filePath -> FileMatches.from(filePath, isMatching))
                     .peek(partial -> partial.ifFailure(System.err::println))
                     .mapMulti(Partial<FileMatches, IOException>::ifSuccess)
                     .filter(matches -> !matches.matchingLines().isEmpty())
-                    .peek(matches -> System.out.println(matches.fileName()))
+                    .peek(matches -> System.out.println(matches.filePath()))
                     .map(FileMatches::matchingLines)
                     .mapMulti(List::forEach)
                     .forEach(System.out::println);
