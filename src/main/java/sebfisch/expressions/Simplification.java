c45e20a25042b6d3e03219deaf2614afd470aba7
Sebastian Fischer <mail@sebfisch.de>
2023-11-17 16:25:33 +0100

    add more simplifications

--- src/main/java/sebfisch/expressions/Simplification.java
+++ src/main/java/sebfisch/expressions/Simplification.java
@@ -8,6 +8,8 @@ public final class Simplification {
     public static final Transform TRANSFORM
             = Transform.combineAll(
                     Simplification::normalizeConst,
+                    Simplification::removeNeg,
+                    Simplification::removeNeutral,
                     Simplification::cancelMul
             ).recursively();
 
@@ -18,6 +20,36 @@ public final class Simplification {
                 Expr.Small.ZERO;
             case Expr.Num(var value) when value == 1 ->
                 Expr.Small.ONE;
+            default ->
+                expr;
+        };
+    }
+
+    private static Expr removeNeg(Expr expr) {
+        return switch (expr) {
+            case Expr.Neg(Expr.Neg(var e)) ->
+                e;
+            case Expr.Add(var l, Expr.Neg(var r)) ->
+                new Expr.Sub(l, r);
+            case Expr.Sub(var l, Expr.Neg(var r)) ->
+                new Expr.Add(l, r);
+            default ->
+                expr;
+        };
+    }
+
+    private static Expr removeNeutral(Expr expr) {
+        return switch (expr) {
+            case Expr.Add(var l, var r) when l == Expr.Small.ZERO ->
+                r;
+            case Expr.Add(var l, var r) when r == Expr.Small.ZERO ->
+                l;
+            case Expr.Sub(var l, var r) when l == Expr.Small.ZERO ->
+                new Expr.Neg(r);
+            case Expr.Sub(var l, var r) when r == Expr.Small.ZERO ->
+                l;
+            case Expr.Mul(var l, var r) when l == Expr.Small.ONE ->
+                r;
             default ->
                 expr;
         };
